a982b3243e663fa619435c4dcd10508857d08c13
David Schmidt <deschmidt@detr.nv>
2025-10-22 10:45:21 -0700

    BLS download updates

--- R/get_latest_qcew_data.R
+++ R/get_latest_qcew_data.R
@@ -1,4 +1,4 @@
-#' Get the Most Recent BLS QCEW Industry Data (with caching and validation)
+#' Get the Most Recent BLS QCEW Industry Data (with caching and BLS headers)
 #'
 #' Retrieves the most recent available **Quarterly Census of Employment and Wages (QCEW)** CSV file.
 #' Looks back two to three quarters (BLS data typically lags) and downloads the first available file.
@@ -13,12 +13,10 @@
 #'   \item{check_year}{The year of the data returned.}
 #' }
 #' @export
-get_latest_qcew_data <- function(industry_code = "10", quarters_to_check = c(2,3)) {
+get_latest_qcew_data <- function(industry_code = "10", quarters_to_check = c(2, 3)) {
   current_date <- Sys.Date()
   current_year <- lubridate::year(current_date)
   current_quarter <- lubridate::quarter(current_date)
-
-  cache_dir <- get_cache_dir()
 
   for (offset in quarters_to_check) {
     check_quarter <- current_quarter - offset
@@ -29,22 +27,24 @@ get_latest_qcew_data <- function(industry_code = "10", quarters_to_check = c(2,3
       check_year <- current_year
     }
 
-    url <- paste0("https://data.bls.gov/cew/data/api/", check_year, "/", check_quarter, "/industry/", industry_code, ".csv")
-    destfile <- file.path(cache_dir, paste0("bls_", check_year, "_Q", check_quarter, ".csv"))
+    url <- paste0(
+      "https://www.bls.gov/cew/data/api/",
+      check_year, "/", check_quarter,
+      "/industry/", industry_code, ".csv"
+    )
 
-    # Remove directory if it exists at file path
-    if (dir.exists(destfile)) unlink(destfile, recursive = TRUE)
+    # Attempt to download/cache the file
+    local_file <- tryCatch(
+      download_if_new(url),
+      error = function(e) NULL
+    )
 
-    # HEAD check
-    head_resp <- httr::HEAD(url)
-    if (httr::status_code(head_resp) != 200) next
-
-    download_if_new(url, destfile, check = "size")
-
-    if (file.exists(destfile)) {
+    # If file exists, read and return
+    if (!is.null(local_file) && file.exists(local_file)) {
       message("Returning data for ", check_year, " Q", check_quarter)
+      data <- data.table::fread(local_file)
       return(list(
-        data = data.table::fread(destfile),
+        data = data,
         check_quarter = check_quarter,
         check_year = check_year
       ))
